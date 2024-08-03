9eb3b1ebc77efb55677ed84716a54e75b0e406f8
GayatriXJadhav <gayatrijadhav1202@gmail.com>
2024-08-03 23:25:36 +0530

    About and Hero section changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/kevinRushProfile.png";
 const Hero = () => {
     return (
         <div className="border-b border-neutral-900 pb-4 lg:mb-35">
@@ -7,6 +9,17 @@ const Hero = () => {
                         <h1 className="pb-16 text-6xl font-thin tracking-light lg:mt-16 lg:text-8xl">
                             Gayatri Jadhav
                         </h1>
+                        <span className="bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                            Full Stack Developer
+                        </span>
+                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                            {HERO_CONTENT}
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img src={profilePic} alt="Kevin Rush" />
                     </div>
                 </div>
             </div>
